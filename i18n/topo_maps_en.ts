bad4f076442b3dc31a37c1c84e5dd1b668a56b46
shingo405nagano <makishin.nagano@gmail.com>
2024-09-12 23:25:27 +0900

    [update&new]

---
+++ i18n/topo_maps_en.ts
@@ -0,0 +1,332 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="ja_JP">
+<context>
+    <name>GeneratingTopographyDialogBase</name>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="14"/>
+        <source>DTMから地形図を作成する</source>
+        <translation type="unfinished">DTM to TopoMaps</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="33"/>
+        <source>Slope</source>
+        <translation type="unfinished">Slope</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="39"/>
+        <source>Slope （傾斜） </source>
+        <translation type="unfinished">Slope Options</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="51"/>
+        <source>リサンプリング</source>
+        <translation type="unfinished">Resampling</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="87"/>
+        <source>リサンプリング後の分解能</source>
+        <translation type="unfinished">Resolution</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="112"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;※分解能が高い場合はリサンプリングする事でノイズの影響を抑える事ができます。 例）1m -&amp;gt; 10m&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">If the resolution is high, the effect of noise can be suppressed by resampling. Example: 1m -&gt; 10m</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="953"/>
+        <source>ガウシアンフィルターを適用する</source>
+        <translation type="unfinished">Gaussian filter</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="996"/>
+        <source>Sigma</source>
+        <translation type="unfinished">Sigma</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1003"/>
+        <source>Hint</source>
+        <translation type="unfinished">Hint</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="578"/>
+        <source>相対的な透過率の変更</source>
+        <translation type="unfinished">Change in relative transmittance</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="602"/>
+        <source>%</source>
+        <translation type="unfinished">%</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="264"/>
+        <source>TPI</source>
+        <translation type="unfinished">TPI</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="270"/>
+        <source>TPI （Topographic Position Index ： 地形位置指数）</source>
+        <translation type="unfinished">TPI Options ( TopographicPositionIndex )</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="287"/>
+        <source>※TPIの設定を行わない場合は、隣接セルを使用してTPIの計算を行います。
+</source>
+        <translation type="unfinished">If TPI is not set, adjacent cells are used to calculate TPI.</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="298"/>
+        <source>畳み込み用カーネル設定</source>
+        <translation type="unfinished">Convolutional Options</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="328"/>
+        <source>カーネルサイズを距離で指定</source>
+        <translation type="unfinished">Kernel size specified by distance</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="333"/>
+        <source>カーネルサイズをセル数で指定</source>
+        <translation type="unfinished">Kernel size specified by cells</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="372"/>
+        <source>片側</source>
+        <translation type="unfinished">One size</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="433"/>
+        <source>平均</source>
+        <translation type="unfinished">Mean</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="440"/>
+        <source>ドーナツ</source>
+        <translation type="unfinished">Doughnut</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="450"/>
+        <source>ガウシアン</source>
+        <translation type="unfinished">Gaussian</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="457"/>
+        <source>4方向の端</source>
+        <translation type="unfinished">4-Direction</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="467"/>
+        <source>逆ガウシアン</source>
+        <translation type="unfinished">Inverse Gaussian</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="474"/>
+        <source>8方向の端</source>
+        <translation type="unfinished">8-Direction</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="484"/>
+        <source>隣接セルを使用</source>
+        <translation type="unfinished">Adjacent cells</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="759"/>
+        <source>外れ値処理</source>
+        <translation type="unfinished">Outlier treatment</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="530"/>
+        <source>IQR × </source>
+        <translation type="unfinished">IQR =</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="664"/>
+        <source>TRI</source>
+        <translation type="unfinished">TRI</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="670"/>
+        <source>TRI （Terrain Ruggedness Index ： 地形凹凸指数） </source>
+        <translation type="unfinished">TRI Options ( Terrain Ruggedness Index )</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="691"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS UI Gothic&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; -qt-user-state:65536;&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;※分解能が高い場合は TRI を使用した方が地形の変化がわかりやすくなります （1mなど）。分解能が低い場合は必要ありません。&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished">*If the resolution is high, using TRI will make it easier to see changes in topography (e.g., 1 m). Not necessary if the resolution is low.</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="741"/>
+        <source>IQR ×</source>
+        <translation type="unfinished">IQR = </translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="788"/>
+        <source>Hillshade</source>
+        <translation type="unfinished">Hillshade</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="794"/>
+        <source>Hillshade （陰影起伏図） </source>
+        <translation type="unfinished">Hillshade Options</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="821"/>
+        <source>強調係数</source>
+        <translation type="unfinished">Exaggeration</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="847"/>
+        <source>光源タイプ</source>
+        <translation type="unfinished">Shading</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="873"/>
+        <source>高度</source>
+        <translation type="unfinished">Altitude of light</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="883"/>
+        <source>方位</source>
+        <translation type="unfinished">Aspect</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="916"/>
+        <source>Single</source>
+        <translation type="unfinished">Single</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="921"/>
+        <source>Multiple</source>
+        <translation type="unfinished">Multiple</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="940"/>
+        <source>傾斜も使用する</source>
+        <translation type="unfinished">Use slope</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1045"/>
+        <source>Log</source>
+        <translation type="unfinished">Log</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1060"/>
+        <source>キャンセル</source>
+        <translation type="unfinished">Cancel</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1067"/>
+        <source>実行</source>
+        <translation type="unfinished">Execute</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1100"/>
+        <source>入力設定</source>
+        <translation type="unfinished">Input</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1132"/>
+        <source>ファイルを読み込む</source>
+        <translation type="unfinished">Read File</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1145"/>
+        <source>レイヤーを読み込む</source>
+        <translation type="unfinished">Read Layer</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1181"/>
+        <source>DTM （DEM） ファイル</source>
+        <translation type="unfinished">DTM ( DEM )</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1193"/>
+        <source>DTM （DEM） レイヤー</source>
+        <translation type="unfinished">DTM ( DEM )</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1242"/>
+        <source>分解能</source>
+        <translation type="unfinished">Resolution</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1252"/>
+        <source>最初にリサンプリングを行う</source>
+        <translation type="unfinished">Resampling first</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1274"/>
+        <source>アルゴリズム</source>
+        <translation type="unfinished">Algorithm</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1285"/>
+        <source>Cubic Spline</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1290"/>
+        <source>Cubic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1295"/>
+        <source>Bilinear</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1300"/>
+        <source>Nearest Neighbour</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1340"/>
+        <source>出力設定</source>
+        <translation type="unfinished">Output</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1372"/>
+        <source>ヴィンテージ</source>
+        <translation type="unfinished">Vintage</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1379"/>
+        <source>スタイルの確認</source>
+        <translation type="unfinished">Show Styles</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1402"/>
+        <source>CS立体図風</source>
+        <translation type="unfinished">CS-Map</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1412"/>
+        <source>RGB</source>
+        <translation type="unfinished">RGB</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1452"/>
+        <source>サンプルの計算</source>
+        <translation type="unfinished">Sample</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1464"/>
+        <source>プロジェクトにレイヤーを追加</source>
+        <translation type="unfinished">Add layer for project</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1509"/>
+        <source>出力ファイル</source>
+        <translation type="unfinished">Output file</translation>
+    </message>
+    <message>
+        <location filename="../views/generate_topography_dialog_base.ui" line="1558"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
